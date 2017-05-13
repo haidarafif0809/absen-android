4f584883c5e35d88c8c6a97defcd10ad58368ee5
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-05-13 10:05:53 +0700

    Penambahan variable yang di butuhkan untuk olah data absen

--- app/src/main/java/com/example/haidar/toko/Config.java
+++ app/src/main/java/com/example/haidar/toko/Config.java
@@ -6,26 +6,59 @@ package com.example.haidar.toko;
 
 public class Config {
     //Address of our scripts of the CRUD
-    public static final String URL_ADD="http://192.168.94.1/Android/CRUD/addEmp.php";
-    public static final String URL_GET_ALL = "http://192.168.94.1/Android/CRUD/getAllEmp.php";
-    public static final String URL_GET_EMP = "http://192.168.94.1/Android/CRUD/getEmp.php?id=";
-    public static final String URL_UPDATE_EMP = "http://192.168.94.1/Android/CRUD/updateEmp.php";
-    public static final String URL_DELETE_EMP = "http://192.168.94.1/Android/CRUD/deleteEmp.php?id=";
+
+    public static final String urlAplikasi = "http://192.168.1.16/absen_android";
+    public static final String URL_ADD_LOKASI = urlAplikasi+"/android/tambah_lokasi.php";
+    public static final String URL_GET_ALL_LOKASI = urlAplikasi+"/android/data_lokasi.php";
+    public static final String URL_GET_LOKASI = urlAplikasi+"/android/pilih_lokasi.php?id=";
+    public static final String URL_UPDATE_LOKASI = urlAplikasi+"/android/update_lokasi.php";
+    public static final String URL_DELETE_LOKASI = urlAplikasi+"/android/hapus_lokasi.php?id=";
+
+    public static final String URL_VALIDASI_LOGIN_ADMIN = urlAplikasi+"/android/validasi_login_admin.php?id=";
+    public static final String URL_PROSES_ABSEN_MASUK = urlAplikasi+"/android/absen_masuk.php";
+    public static final String URL_PROSES_ABSEN_PULANG = urlAplikasi+"/android/absen_pulang.php";
+
+    public static final String URL_ADD_USER = urlAplikasi+"/android/tambah_user.php";
+
 
     //Keys that will be used to send the request to php scripts
-    public static final String KEY_EMP_ID = "id";
-    public static final String KEY_EMP_NAME = "name";
-    public static final String KEY_EMP_DESG = "desg";
-    public static final String KEY_EMP_SAL = "salary";
+    public static final String KEY_LOKASI_ID = "id";
+    public static final String KEY_LATITUDE = "latitude";
+    public static final String KEY_LONGITUDE = "longitude";
+    public static final String KEY_NAMA_LOKASI = "nama";
+
+
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_NAMA = "nama";
+
+    //proses absen
+    public static final String KEY_NIK = "nik";
+    public static final String KEY_LOKASI = "lokasi";
+    public static final String KEY_BATAS_JARAK = "batas_jarak_absen";
+
+
+
 
     //JSON Tags
     public static final String TAG_JSON_ARRAY="result";
-    public static final String TAG_ID = "id";
-    public static final String TAG_NAME = "name";
-    public static final String TAG_DESG = "desg";
-    public static final String TAG_SAL = "salary";
+    public static final String TAG_LOKASI_ID = "id";
+    public static final String TAG_LATITUDE = "latitude";
+    public static final String TAG_LONGITUDE = "longitude";
+    public static final String TAG_NAMA_LOKASI = "nama";
+    public static final String TAG_BATAS_JARAK = "batas_jarak_absen";
 
-    //employee id to pass with intent
-    public static final String EMP_ID = "emp_id";
+    public static final String TAG_USERNAME = "username";
+    public static final String TAG_PASSWORD = "password";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_NIK = "nik";
+    public static final String TAG_ID = "id";
+
+    //lokasi id to pass with intent
+    public static final String LOKASI_ID = "id";
+    public static final String USER_ID = "id";
+
+
+
 
 }
