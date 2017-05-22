40a05c2c73f51c3ac0bc341c27bf9926883324c0
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-05-22 13:48:06 +0700

    File untuk daftar karyawan hadir

---
+++ app/src/main/java/com/example/haidar/toko/UserHadirActivity.java
@@ -0,0 +1,13 @@
+package com.example.haidar.toko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserHadirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_hadir);
+    }
+}
