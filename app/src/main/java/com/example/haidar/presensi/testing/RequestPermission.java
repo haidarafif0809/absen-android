513097a3f2fc62e508eef7e0f692f37be3924a44
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-08-16 22:03:59 -0700

    Testing Request Permission pakai package

--- app/src/main/java/com/example/haidar/presensi/testing/RequestPermission.java
+++ app/src/main/java/com/example/haidar/presensi/testing/RequestPermission.java
@@ -1,15 +1,87 @@
 package com.example.haidar.presensi.testing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.haidar.presensi.R;
+import com.kishan.askpermission.AskPermission;
+import com.kishan.askpermission.ErrorCallback;
+import com.kishan.askpermission.PermissionCallback;
+import com.kishan.askpermission.PermissionInterface;
 
-public class RequestPermission extends AppCompatActivity {
+public class RequestPermission extends AppCompatActivity  implements PermissionCallback, ErrorCallback {
+
+    private static final String TAG = "DemoActivity";
+    private static final int REQUEST_PERMISSIONS = 20;
+    private Button reqButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_permission);
+        reqButton = (Button) findViewById(R.id.requesButton);
+        reqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reqPermission();
+            }
+        });
+
+    }
+
+    private void reqPermission() {
+        new AskPermission.Builder(this).setPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .setCallback(this)
+                .setErrorCallback(this)
+                .request(REQUEST_PERMISSIONS);
+    }
+
+
+    @Override
+    public void onShowRationalDialog(final PermissionInterface permissionInterface, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("We need permissions for this app.");
+        builder.setPositiveButton("oke", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                permissionInterface.onDialogShown();
+            }
+        });
+        builder.setNegativeButton("cancel", null);
+        builder.show();
+    }
+
+    @Override
+    public void onShowSettings(final PermissionInterface permissionInterface, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("We need permissions for this app. Open setting screen?");
+        builder.setPositiveButton("oke", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                permissionInterface.onSettingsShown();
+            }
+        });
+        builder.setNegativeButton("cancel", null);
+        builder.show();
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode) {
+        Toast.makeText(this, "Permissions Received.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode) {
+        Toast.makeText(this, "Permissions Denied.", Toast.LENGTH_LONG).show();
     }
 }
